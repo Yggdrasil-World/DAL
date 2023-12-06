454ea68d84aa7f0ee637b69a44d07e0e72ed9549
Invincible2211 <fynn.schur@web.de>
2023-12-06 13:24:36 +0100

    updatet method naming

--- src/main/java/de/yggdrasil/core/dal/data/datasources/models/ConfigEntry.java
+++ src/main/java/de/yggdrasil/core/dal/data/datasources/models/ConfigEntry.java
@@ -2,8 +2,10 @@ package de.yggdrasil.core.dal.data.datasources.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.bytemc.evelon.repository.annotations.Entity;
 import net.bytemc.evelon.repository.annotations.PrimaryKey;
 
+@Entity(name = "config")
 @AllArgsConstructor
 @Getter
 public class ConfigEntry {
--- src/main/java/de/yggdrasil/core/dal/pipeline/implementations/DefaultConfigPipeline.java
+++ src/main/java/de/yggdrasil/core/dal/pipeline/implementations/DefaultConfigPipeline.java
@@ -11,14 +11,14 @@ public class DefaultConfigPipeline implements
         ReadPipeline<ConfigReadRequest, ConfigResponse>, WritePipeline<ConfigWriteRequest> {
 
     @Override
-    public ConfigResponse readBytes(ConfigReadRequest readRequest) {
+    public ConfigResponse readData(ConfigReadRequest readRequest) {
         return new ConfigResponse(DatasourceLibrary.get()
                 .getDatasource(readRequest.getDatasourceClass())
                 .getData(readRequest.getIdentifier()));
     }
 
     @Override
-    public void writeBytes(ConfigWriteRequest writeRequest) {
+    public void writeData(ConfigWriteRequest writeRequest) {
         DatasourceLibrary.get().getDatasource(writeRequest.getDatasourceClass())
                 .writeData(writeRequest.getKey(), writeRequest.getData());
     }
