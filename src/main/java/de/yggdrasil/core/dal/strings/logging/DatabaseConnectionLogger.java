06cfa6efae83b227882f24d8b64e878d39342923
Invincible2211 <fynn.schur@web.de>
2024-03-15 22:08:59 +0100

    added db credential support vor evelon module

---
+++ src/main/java/de/yggdrasil/core/dal/strings/logging/DatabaseConnectionLogger.java
@@ -0,0 +1,7 @@
+package de.yggdrasil.core.dal.strings.logging;
+
+public interface DatabaseConnectionLogger {
+
+    String DATABASE_CONNECTION_DATA = "Connected to Postgres Database with Host: %s Port: %s Used Database: %s with User: %s";
+
+}
